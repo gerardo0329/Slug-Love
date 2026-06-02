20878218f1c1e8dc3386077d109910471a627e99
gerardo0329 <gerardo032901@gmail.com>
2026-06-02 11:52:40 -0700

    New

--- ssc.js
+++ ssc.js
@@ -1,19 +1,3 @@
 $("#btn-load").click(function () {
-  window.location.href='SSC.html';
-});
-
-$("#btn-load").click(function () {
-  window.location.href='aps.html';
-});
-
-$("#btn-load").click(function () {
-  window.location.href='SSC.html';
-});
-
-$("#btn-load").click(function () {
-  window.location.href='SSC.html';
-});
-
-$("#btn-load").click(function () {
-  window.location.href='SSC.html';
+  window.location.href='ssc.html';
 });
